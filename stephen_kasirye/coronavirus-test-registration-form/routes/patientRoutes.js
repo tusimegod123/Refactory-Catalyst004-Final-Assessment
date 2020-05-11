c54b24083fe35027b79ac6dd69a671389b2fab43
skcodeug <60166232+skcodeug@users.noreply.github.com>
2020-05-11 18:59:42 +0300

    Final touches to routes file

--- stephen_kasirye/coronavirus-test-registration-form/routes/patientRoutes.js
+++ stephen_kasirye/coronavirus-test-registration-form/routes/patientRoutes.js
@@ -8,39 +8,16 @@ router.get("/", (req, res) => {res.render('login');});
 
 // REGISTER STUDENT
 router.get("/addPatient", (req, res) => {res.render('patientRegistration');});
+
 router.post("/addPatient", async (req, res) => {
     try {
         var newPatient = new Patient(req.body);
         await newPatient.save();
-        res.render("successfulRegistration")
-        // const onePatient = {
-        //     name:"",
-        //     gender:"",
-        //     age:"",
-        //     country:"",
-        //     residence:"",
-        //     phoneNumber:"",
-        //     email:""
-        // };
-        // // res.redirect('/profile');
-        // let x = newStudent.surName + " " + newStudent.givenName; oneStudent.name = x;
-        // x = newStudent.gender; oneStudent.gender = x;
-        // x = newStudent.dob; oneStudent.age = x;
-        // x = newStudent.country; oneStudent.country = x;
-        // x = newStudent.residence; oneStudent.residence = x;
-        // x = newStudent.phoneNumber; oneStudent.phoneNumber = x;
-        // x = newStudent.email; oneStudent.email = x;
-        // res.render('profile', {student: oneStudent});
+        res.render("successfulRegistration");
     } catch (error) {
         console.log(error);
         res.send("Unable to save to Database");
     }
-});
-
-// PROFILE PAGE
-router.post("/profile", (req, res) => {
-
-    res.render('profile');
 });
 
 // LOGOUT
@@ -48,9 +25,5 @@ router.post("/logout", (req, res) => {
     req.session.user;
     logout();
 });
-
-// HTML FILE BUILD & TEST ENDPOINT
-router.get("/test", (req, res) => {res.sendFile(path.join(__dirname, '../views/html_files', 'patientRegistration.html'));});
-router.get("/test2", (req, res) => {res.sendFile(path.join(__dirname, '../views/html_files', 'successfulRegistration.html'));});
 
 module.exports = router;
