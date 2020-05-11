3443511693482e376334b23cf721e692dcef0250
briantical <lutbrianivan@gmail.com>
2020-05-11 12:16:40 +0300

    Configured the database

--- lutaaya_brian_ivan/coronavirus_test_registration_form/src/index.js
+++ lutaaya_brian_ivan/coronavirus_test_registration_form/src/index.js
@@ -1,12 +1,18 @@
 // import all the dependencies
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 
 // Declare the constants
 const PORT = 5000;
+const DB_URI = 'mongodb://localhost:27017/coronavirus';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
 // Initialize the application
 const app = express();
@@ -24,6 +30,11 @@ app.set('views', path.join(__dirname, '/views'));
 
 // The endpoints
 app.use('/', routes);
+
+mongoose.connect(DB_URI, options, (error) => {
+  if (error) throw error;
+  console.log(`The Database is running ...`);
+});
 
 app.listen(PORT, () => {
   console.log(`The server is listening on PORT ${PORT}`);
