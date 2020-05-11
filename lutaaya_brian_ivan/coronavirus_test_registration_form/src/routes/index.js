aa184d980903c4cfc60815fe20f40669da693d9d
briantical <lutbrianivan@gmail.com>
2020-05-11 12:46:51 +0300

    Defined the application routes

--- lutaaya_brian_ivan/coronavirus_test_registration_form/src/routes/index.js
+++ lutaaya_brian_ivan/coronavirus_test_registration_form/src/routes/index.js
@@ -1,11 +1,34 @@
 // Import the dependencies
 const Router = require('express').Router;
+const Patient = require('./../models/model');
 
+// Create an instance of router
 const routes = Router();
 
 // a GET route at '/'
 routes.get('/', (req, res) => {
   res.render('index');
+});
+
+// a POST route at '/'
+routes.post('/', async (req, res) => {
+  try {
+    const patient = new Patient(req.body);
+    await customer.save();
+
+    // Define a response message on success
+    const response = { message: 'Patient successfully registerd', type: 'success' };
+    res.render('index', { response });
+  } catch (error) {
+    // Define a response message on failure
+    const response = { message: 'Patient not registered', type: 'failure' };
+    res.render('index', { response });
+  }
+});
+
+// Redirect users to '/' if a route is undefined
+routes.get('*', (req, res) => {
+  res.redirect('/');
 });
 
 module.exports = routes;
