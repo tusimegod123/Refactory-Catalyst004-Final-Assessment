31771a886bdaff1b0d973cde18415c3695a43695
skcodeug <60166232+skcodeug@users.noreply.github.com>
2020-05-11 18:56:26 +0300

    completed validation

--- stephen_kasirye/coronavirus-test-registration-form/public/js/patientValidation.js
+++ stephen_kasirye/coronavirus-test-registration-form/public/js/patientValidation.js
@@ -1,6 +1,6 @@
 formValidation = () => {
     
-    // Receive Values
+    // STORE ELEMENTS
     var surName = document.registrationForm.surName;
     var givenName = document.registrationForm.givenName;
     var dob = document.registrationForm.dob;
@@ -9,7 +9,7 @@ formValidation = () => {
     var nationality = document.registrationForm.nationality;
     var category = document.registrationForm.category;
 
-    // Reset Borders
+    // RESET BORDERS
     surName.style.border = "1px solid lightgrey"
     givenName.style.border = "1px solid lightgrey"
     dob.style.border = "1px solid lightgrey"
@@ -18,7 +18,7 @@ formValidation = () => {
     nationality.style.border = "1px solid lightgrey"
     category.style.border = "1px solid lightgrey"
 
-    // Reset Error Messages
+    // RESET ERROR MESSAGES
     document.getElementById('surName-error').style.display = "none"
     document.getElementById('givenName-error').style.display = "none"
     document.getElementById('dob-error').style.display = "none"
@@ -27,7 +27,7 @@ formValidation = () => {
     document.getElementById('nationality-error').style.display = "none"
     document.getElementById('category-error').style.display = "none"
 
-    // Object to store confirmations
+    // OBJECT TO STORE VALIDATIONS
     const validations = {
         surName: 0,
         givenName: 0,
@@ -38,7 +38,7 @@ formValidation = () => {
         category: 0
     };
 
-    // Run Validations
+    // RUN VALIDATIONS
     validations.surName = surName_validation(surName, 1, 16);
     validations.givenName = givenName_validation(givenName, 1, 16);
     validations.dob = dob_validation(dob, 1, 150);
@@ -47,10 +47,10 @@ formValidation = () => {
     validations.nationality = nationality_validation(nationality, 5, 20);
     validations.category = category_validation(category);
 
-    // Convert to Array
+    // CONVERT TO ARRAY
     const values = Object.values(validations)
 
-    // Examine Array
+    // EXAMINE ARRAY
     var form_validity = 0
     for(i=0; i<7; i++) {
         if (values[i] === 1){
@@ -58,7 +58,7 @@ formValidation = () => {
         } 
     }
 
-    // Determine completion
+    // ASSESS COMPLETION
     if (form_validity !== 7) {
         // Pause form execution if all fields are not validated in the object literal above
         event.preventDefault();
@@ -69,9 +69,7 @@ formValidation = () => {
         event.currentTarget.submit();
         return true;
     }
-    event.preventDefault();
     
-    alert('Congratulations; validation is done.');
     return true;
 }
 
@@ -103,26 +101,7 @@ givenName_validation = (givenName, min, max) => {
 }
 
 dob_validation = (dob, min, max) => {
-    // if(doB.value.length === 0){
-    //     doB.style.border = "2px solid red"
-    //     document.getElementById('dob-error').style.display = "block"
-    //     doB.focus()
-    //     return 0;
-    // }
-    // else if(doB.value.length <mx||doB.value.length >my){
-    //     doB.style.border = "2px solid red"
-    //     document.getElementById('dob-error').style.display = "none"
-    //     document.getElementById('dob-error').style.display = "block"
-    //     doB.focus()
-    //     return 0;
-    // }
-    // else{ 
-    //     doB.style.border = "2px solid green"
-    //     document.getElementById('dob-error').style.display = "none"
-    //     return 1;
-    //  }
-
-     if(dob.value === ""){
+    if(dob.value === ""){
         dob.style.border = "2px solid red"
         document.getElementById('dob-error').style.display = "block";
         return 0
@@ -159,65 +138,41 @@ residence_validation = (residence, min, max) => {
      }
 }
 
-occupation_validation = (occupation,mx,my) => {
-    if(occupation.value.length === 0){
-        occupation.style.border = "2px solid red"
-        document.getElementById('occupation-error').style.display = "block"
-        occupation.focus()
-        return 0;
-    }
-    else if(occupation.value.length <mx||occupation.value.length >my){
-        occupation.style.border = "2px solid red"
-        document.getElementById('occupation-error').style.display = "none"
-        document.getElementById('occupation-error').style.display = "block"
-        occupation.focus()
+occupation_validation = (occupation, min, max) => {
+    var letters = /^[A-Za-z]+$/;
+    if(occupation.value.length <= min || occupation.value.length >= max || !occupation.value.match(letters)){
+        occupation.style.border = "2px solid red";
+        document.getElementById('occupation-error').style.display = "block";
         return 0;
     }
     else{ 
-        occupation.style.border = "2px solid green"
-        document.getElementById('occupation-error').style.display = "none"
+        occupation.style.border = "2px solid green";
         return 1;
-     }
+    }
 }
 
-nationality_validation = (nationality,mx,my) => {
-    if(nationality.value.length === 0){
+nationality_validation = (nationality, min, max) => {
+    var letters = /^[A-Za-z]+$/;
+    if(nationality.value.length <= min || nationality.value.length >= max || !nationality.value.match(letters)){
         nationality.style.border = "2px solid red"
-        document.getElementById('nationality-error').style.display = "block"
-        nationality.focus()
-        return 0;
-    }
-    else if(nationality.value.length <mx||nationality.value.length >my){
-        nationality.style.border = "2px solid red"
-        document.getElementById('nationality-error').style.display = "none"
         document.getElementById('nationality-error').style.display = "block"
         nationality.focus()
         return 0;
     }
     else{ 
         nationality.style.border = "2px solid green"
-        document.getElementById('nationality-error').style.display = "none"
         return 1;
      }
 }
 
-category_validation = (category,mx,my) => {
-    if(category.value === "Default"){
+category_validation = (category) => {
+    if (category.value == "Default") {
         category.style.border = "2px solid red"
-        document.getElementById('category-error').style.display = "block"
-        category.focus()
+        document.getElementById('category-error').style.display = "block";
         return 0;
     }
-    else if(category.value.length <mx||category.value.length >my){
-        category.style.border = "2px solid red"
-        document.getElementById('category-error').style.display = "none"
-        document.getElementById('category-error').style.display = "block"
-        category.focus()
-        return 0;
-    }
-    else{ 
-        category.style.border = "2px solid green"
-        document.getElementById('category-error').style.display = "none"
+    else {
+        category.style.border = "1px solid green"
         return 1;
-     }
+    }
 }
